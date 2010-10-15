c0d81ceacc0d47038d613a2591598f7e9be3109b
Lenart Rudel <lenart.rudel@gmail.com>
2010-10-15 22:56:20 +0200

    Use whenever for cron tasks.

--- config/environment.rb
+++ config/environment.rb
@@ -27,6 +27,7 @@ Rails::Initializer.run do |config|
   config.gem "friendly_id", :version => ">= 3.0.2"
   config.gem 'thinking-sphinx', :lib => 'thinking_sphinx', :version => '>=1.3.16'
   config.gem "ambethia-recaptcha", :lib => "recaptcha/rails", :source => "http://gems.github.com"
+  config.gem "whenever"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
